7e731108518c892a6f72bfe9681ed46d62520e51
Sejuti Sharmin <ssmethila@live.com>
2023-03-05 01:06:28 +0600

    Initial Commit

---
+++ Random Photo Generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random Photo Generator
+//
+//  Created by Sejuti Sharmin on 5/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
